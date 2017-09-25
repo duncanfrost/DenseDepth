39bec054e045ddbc053cb00c57a44a1c55145319
Duncan Frost <frosd01@gmail.com>
2017-09-25 10:14:29 +0100

    More cudaimage implementation

--- ORUtils/CUDAImage.h
+++ ORUtils/CUDAImage.h
@@ -2,34 +2,52 @@
 #include <cuda_runtime_api.h>
 #include <cuda_runtime.h>
 
+template <typename T>
 class CUDAImage
 {
 public:
-
     CUDAImage(int rows, int columns)
+        rows(rows), columns(columns)
     {
         nElements = rows*columns;
-        cudaMallocHost((void**)&cpuData, nElements * sizeof(unsigned char));
-        cudaMalloc((void**)&cudaData, nElements * sizeof(unsigned char));
+        cudaMallocHost((void**)&cpuData, nElements * sizeof(T));
+        cudaMalloc((void**)&cudaData, nElements * sizeof(T));
     }
 
     void UpdateDeviceFromHost()
     {
-        cudaMemcpy(cudaData, cpuData, nElements * sizeof(unsigned char), cudaMemcpyHostToDevice);
+        cudaMemcpy(cudaData, cpuData, nElements * sizeof(T), cudaMemcpyHostToDevice);
     }
 
     void UpdateHostFromDevice()
     {
-        cudaMemcpy(cudaData, cpuData, nElements * sizeof(unsigned char), cudaMemcpyHostToDevice);
-        cudaMemcpy(cpuData, cudaData, nElements * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+        cudaMemcpy(cpuData, cudaData, nElements * sizeof(T), cudaMemcpyDeviceToHost);
     }
 
+    unsigned char *GetCPUData() const
+    {
+        return cpuData;
+    }
+
+    unsigned char *GetCUDAData() const
+    {
+        return cpuData;
+    }
+
+    void Initialise(T val)
+    {
+        for (unsigned int y = 0; y < rows; y++)
+            for (unsigned int x = 0; x < columns; x++)
+            {
+                int index = x + columns*y;
+                cpuData[index] = val;
+            }
+    }
 
 private:
-
     unsigned int nElements;
+    unsigned int rows, columns;
 
-    unsigned char *cpuData;
-    unsigned char *cudaData;
-
+    T *cpuData;
+    T *cudaData;
 };
