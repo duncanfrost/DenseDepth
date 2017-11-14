a56fdcdebc0b15f7961847bbb041267814efe260
Duncan Frost <frosd01@gmail.com>
2017-11-14 11:31:15 +0000

    Added arkit binary

---
+++ bin/arkit.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <GUI/VisualisationModule.h>
+#include <GUI/ARWindow.h>
+#include <GUI/MapWindow.h>
+#include <Engines/MonoEngine.h>
+#include <Tracking/TUMFileTracker.h>
+#include <ImageSource/TUMSource.h>
+#include <ImageSource/TUMDepthSource.h>
+
+void Idle(void);
+void KeyboardFunction(unsigned char key, int x, int y);
+VisualisationModule *visModule;
+MonoEngine *engine;
+TUMSource *source;
+TUMDepthSource *depthSource;
+FileTracker *tracker;
+
+int main(void)
+{
+    std::string filename1 = "/home/duncan/Data/TUM/rgbd_dataset_freiburg2_desk/rgb.txt";
+    std::string filename2 = "/home/duncan/Data/TUM/rgbd_dataset_freiburg2_desk/depth.txt";
+
+
+    std::string poseDirectory = "/home/duncan/Data/TUM/rgbd_dataset_freiburg2_desk/";
+
+    source = new TUMSource(filename1);
+    depthSource = new TUMDepthSource(filename2);
+    tracker = new TUMFileTracker(poseDirectory, "groundtruth.txt");
+
+    MonoEngine::Settings settings;
+    settings.fx = 520.908620;
+    settings.fy = 521.007327;
+    settings.cx = 325.141442;
+    settings.cy = 249.701764;
+
+    engine = new MonoEngine(source, depthSource, tracker, settings);
+
+    visModule = new VisualisationModule(&Idle);
+    visModule->AddWindow(new ARWindow("AR",640,480,engine->GetARData()));
+    visModule->AddWindow(new MapWindow("Map",640,480,engine));
+
+    visModule->SetKeyboardFunction(&KeyboardFunction);
+    visModule->StartLoop();
+}
+
+void Idle(void)
+{
+    engine->Process();
+    visModule->DrawWindows();
+}
+
+void KeyboardFunction(unsigned char key, int x, int y)
+{
+    switch(key) {
+    case 27://esc
+        exit(0);
+        break;
+
+    case ' ':
+        engine->SampleFromBufferMid();
+        break;
+
+    case 'p':
+        engine->TogglePaused();
+        break;
+        
+    case 'o':
+        engine->SmoothPhotoBuffer(200);
+        engine->WritePhotoErrors("/home/duncan/photo.bin");
+        break;
+    }
+}
